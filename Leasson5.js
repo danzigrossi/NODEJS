e1c1f2a961b5c1bfc088215c3c271bc98395c24e
danzigrossi <danfael@gmail.com>
2015-04-07 10:10:32 -0300

    Create Leasson5.js

---
+++ Leasson5.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var path = require('path');
+var file = process.argv[2];
+var filter = process.argv[3];
+var filterLength = process.argv[3].length;
+fs.readdir(file, function (err, buffer) {
+  if (err) throw err;
+
+  for( i = 0 ; i < buffer.length ; i++){
+	var arquivoAtual = buffer[i].toString('utf8');
+	if(arquivoAtual.length < filterLength){ continue;}
+	
+	if(path.extname(arquivoAtual )== ('.'+filter)){
+		console.log(buffer[i].toString('utf8'));
+	}
+  }
+});
